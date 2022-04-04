a157bbaac534ca0d53e36f10f10fc59a183dd79e
Yeon Kim <awyeon@gmail.com>
2022-04-04 15:16:10 +0800

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms
+  resources :users
+  root 'rooms#index'
+
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
