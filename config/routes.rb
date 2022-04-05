eefc4f41976775c8ce2b4c3ed706173d1d5a5f3d
Yeon Kim <awyeon@gmail.com>
2022-04-05 15:33:08 +0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'rooms#index'
-  resources :rooms
-  resources :users
   resources :rooms do
     resources :messages
   end
+  resources :users
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
