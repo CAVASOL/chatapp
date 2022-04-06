df6c29704b3ecdc3d713437983de15ef1a24ea0e
Yeon Kim <awyeon@gmail.com>
2022-04-06 10:15:22 +0800

    public room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   def index
     @current_user = current_user
     redirect_to '/signin' unless @current_user
-    @rooms = Room.public_rooms
+    @rooms = Room.public_rooms.all
     @users = User.all_except(@current_user)
     @user = current_user
 
@@ -16,7 +16,7 @@ class RoomsController < ApplicationController
   def show
     @current_user = current_user
     @single_room = Room.find(params[:id])
-    @rooms = Room.public_rooms
+    @rooms = Room.public_rooms.all
     @users = User.all_except(@current_user)
     @room = Room.new
     @message = Message.new
