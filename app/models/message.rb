d65ebe72d4b5aaef8a524ccdd7d3a1cc67afb127
Yeon Kim <awyeon@gmail.com>
2022-04-06 17:39:41 +0800

    testing

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,8 @@ class Message < ApplicationRecord
   after_create_commit { broadcast_append_to self.room }
 
   def confirm_participant
-    if self.room.is_private
-      is_participant = Participant.where(user_id: self.user.id, room_id: self.room.id).first
+    if confirm_participant.room.is_private
+      is_participant = Participant.where(user_id: confirm_participant.user.id, room_id: confirm_participant.room.id).first
       throw :abort unless is_participant
     end
   end
