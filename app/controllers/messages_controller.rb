8d540e8f90f6eb9636ee91073866b0b8e0da877b
Yeon Kim <awyeon@gmail.com>
2022-04-13 22:03:18 +0800

    fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
   def create
     @current_user = current_user
     @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
-    @messages = Message.all
   end
 
   private
